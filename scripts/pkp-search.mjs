e08f24f875610a0356d5d8e6351a4fc5ab067698
hhglory <hhglory@outlook.com>
2025-08-27 00:55:41 +0800

    hotfix(pkp-search) - change chain id

--- scripts/pkp-search.mjs
+++ scripts/pkp-search.mjs
@@ -1,72 +1,92 @@
 import { greenLog, tableLog } from '../utils.mjs';
-import {LitContracts} from "@lit-protocol/contracts-sdk";
-import {providers, ethers} from "ethers";
+import { LitContracts } from '@lit-protocol/contracts-sdk';
+import { providers, ethers } from 'ethers';
 
 export async function pkpSearchFunc({ args }) {
-    let params = processArgs(args);
-    
-    if (params.help) {
-        greenLog(printHelp());
-    }
-    
-    let rpcProvider = new providers.JsonRpcProvider("https://chain-rpc.litprotocol.com/http", 175177);
-    let contractsClient = bootstrapClient(rpcProvider);
-    
-    await contractsClient.connect();
+  let params = processArgs(args);
 
-    switch(params.get) {
-        case "authMethodScopes":
-            const scopes = await getAuthMethodScopes(contractsClient, params.publicKey, params.authMethodId, params.type, params.maxScope);
-            !params.format && greenLog(`${JSON.stringify(scopes)}`);
-            params.format === "table" && tableLog(scopes);
-            break;
-        
-        case "authMethods":
-            const authMethods = await searchAuthMethods(contractsClient, params.publicKey);
-            !params.format && greenLog(`${JSON.stringify(authMethods)}`);
-            params.format === "table" && tableLog(authMethods);
-            break;
-        
-        case "ethAddr":
-            const ethAddr = await getEthAddress(client, params.publicKey);
-            !params.format && greenLog(`${JSON.stringify(ethAddr)}`);
-            params.format === "table" && tableLog(ethAddr);
-            break;
-        
-        case "tokenIds":
-            const tokenIds = await getTokenIdsByAuthMethod(contractsClient, params.authMethodId, params.clientId, params.appId, params.type);
-            !params.format && greenLog(`${JSON.stringify(tokenIds)}`);
-            params.format === "table" && tableLog(tokenIds);
-            break;
-    }
+  if (params.help) {
+    greenLog(printHelp());
+  }
 
-    process.exit(0);
+  // 175177 is outdated, change it to 175188
+  // https://developer.litprotocol.com/connecting-to-a-lit-network/lit-blockchains/chronicle-yellowstone
+  let rpcProvider = new providers.JsonRpcProvider(
+    'https://chain-rpc.litprotocol.com/http',
+    175188
+  );
+  let contractsClient = bootstrapClient(rpcProvider);
+
+  await contractsClient.connect();
+
+  switch (params.get) {
+    case 'authMethodScopes':
+      const scopes = await getAuthMethodScopes(
+        contractsClient,
+        params.publicKey,
+        params.authMethodId,
+        params.type,
+        params.maxScope
+      );
+      !params.format && greenLog(`${JSON.stringify(scopes)}`);
+      params.format === 'table' && tableLog(scopes);
+      break;
+
+    case 'authMethods':
+      const authMethods = await searchAuthMethods(
+        contractsClient,
+        params.publicKey
+      );
+      !params.format && greenLog(`${JSON.stringify(authMethods)}`);
+      params.format === 'table' && tableLog(authMethods);
+      break;
+
+    case 'ethAddr':
+      const ethAddr = await getEthAddress(client, params.publicKey);
+      !params.format && greenLog(`${JSON.stringify(ethAddr)}`);
+      params.format === 'table' && tableLog(ethAddr);
+      break;
+
+    case 'tokenIds':
+      const tokenIds = await getTokenIdsByAuthMethod(
+        contractsClient,
+        params.authMethodId,
+        params.clientId,
+        params.appId,
+        params.type
+      );
+      !params.format && greenLog(`${JSON.stringify(tokenIds)}`);
+      params.format === 'table' && tableLog(tokenIds);
+      break;
+  }
+
+  process.exit(0);
 }
 
 function processArgs(args) {
-    let params = {}
-    for (let arg of args) {
-        arg = arg.replaceAll("-", "");
-        let parts = arg.split("=");
-        if (parts.length == 2) {
-          params[parts[0]] = parts[1];
-        } else {
-          params[parts[0]] = true;
-        }
+  let params = {};
+  for (let arg of args) {
+    arg = arg.replaceAll('-', '');
+    let parts = arg.split('=');
+    if (parts.length == 2) {
+      params[parts[0]] = parts[1];
+    } else {
+      params[parts[0]] = true;
     }
+  }
 
-    return params;
+  return params;
 }
 
 function bootstrapClient(provider) {
-    let contract = new LitContracts({
-        signer: provider
-    });
-    return contract;
+  let contract = new LitContracts({
+    signer: provider,
+  });
+  return contract;
 }
 
 function printHelp() {
-    const message = `
+  const message = `
 usage: pkp-search <--get=<search-param>> <--pubKey=<public key>> <--authMethodId=<id>> <--authMethodType=<type>> 
     [--clientId=<id>] [-appId=<id>] [--maxScope=<scopeIndex>]
         
@@ -76,53 +96,78 @@ usage: pkp-search <--get=<search-param>> <--pubKey=<public key>> <--authMethodId
         
     search for pkp informaton related to a pkp or auth method meta data.
 `;
-    return message;
+  return message;
 }
 
 async function searchAuthMethods(client, pk) {
-    let tokenId = ethers.utils.keccak256(`0x${pk}`);
-    let authMethods = await client.pkpPermissionsContract.read.getPermittedAuthMethods(tokenId);
-    let authMethodObjs = []
-    for (const authMethod of authMethods) {
-        authMethodObjs.push({
-            authMethodType: authMethod[0]._hex,
-            authMethodiId: authMethod[1],
-            pubKey: authMethod[2],
-        });
-    }
-    return authMethodObjs;
+  let tokenId = ethers.utils.keccak256(`0x${pk}`);
+  let authMethods =
+    await client.pkpPermissionsContract.read.getPermittedAuthMethods(tokenId);
+  let authMethodObjs = [];
+  for (const authMethod of authMethods) {
+    authMethodObjs.push({
+      authMethodType: authMethod[0]._hex,
+      authMethodiId: authMethod[1],
+      pubKey: authMethod[2],
+    });
+  }
+  return authMethodObjs;
 }
 
-async function getAuthMethodScopes(client, pk, authMethodId, authMethodType, maxScope = 3) {
-    let tokenId = ethers.utils.keccak256(`0x${pk}`);
-    let scopes = await client.pkpPermissionsContract.read.getPermittedAuthMethodScopes(tokenId, authMethodType, authMethodId, maxScope);
-    scopes = {
-        scopes
-    };
-    return scopes;
+async function getAuthMethodScopes(
+  client,
+  pk,
+  authMethodId,
+  authMethodType,
+  maxScope = 3
+) {
+  let tokenId = ethers.utils.keccak256(`0x${pk}`);
+  let scopes =
+    await client.pkpPermissionsContract.read.getPermittedAuthMethodScopes(
+      tokenId,
+      authMethodType,
+      authMethodId,
+      maxScope
+    );
+  scopes = {
+    scopes,
+  };
+  return scopes;
 }
 
 async function getEthAddress(client, pk) {
-    let tokenId = ethers.utils.keccak256(`0x${pk}`);
-    let ethAddr = await client.pkpPermissionsContract.read.getEthAddress(tokenId);
-    ethAddr = {
-        address: ethAddr
-    };
+  let tokenId = ethers.utils.keccak256(`0x${pk}`);
+  let ethAddr = await client.pkpPermissionsContract.read.getEthAddress(tokenId);
+  ethAddr = {
+    address: ethAddr,
+  };
 
-    return ethAddr;
+  return ethAddr;
 }
 
-async function getTokenIdsByAuthMethod(client, authMethodId, clientId, appId, type) {
-    if (!authMethodId) {
-        authMethodId = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(`${clientId}:${appId}`));
-    }
+async function getTokenIdsByAuthMethod(
+  client,
+  authMethodId,
+  clientId,
+  appId,
+  type
+) {
+  if (!authMethodId) {
+    authMethodId = ethers.utils.keccak256(
+      ethers.utils.toUtf8Bytes(`${clientId}:${appId}`)
+    );
+  }
 
-    let tokenIds = await client.pkpPermissionsContract.read.getTokenIdsForAuthMethod(type, authMethodId);
-    tokenIds = {
-        tokenIds
-    };
-    !params.format && greenLog(`${JSON.stringify(tokenIds)}`);
-    params.format === "table" && tableLog(tokenIds);
+  let tokenIds =
+    await client.pkpPermissionsContract.read.getTokenIdsForAuthMethod(
+      type,
+      authMethodId
+    );
+  tokenIds = {
+    tokenIds,
+  };
+  !params.format && greenLog(`${JSON.stringify(tokenIds)}`);
+  params.format === 'table' && tableLog(tokenIds);
 
-    return tokenIds;
+  return tokenIds;
 }
